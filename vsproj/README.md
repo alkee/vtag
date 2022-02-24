6a515dcbce8063f834bf993e0fdf47b95f2c1dd0
alkee <alkee.na@gmail.com>
2022-02-24 16:40:41 +0900

    grpc reflection supported

--- vsproj/README.md
+++ vsproj/README.md
@@ -1,3 +1,7 @@
 ## vsproj
 
 projects for visual studio 2022 or higher
+
+each executable(or runnable) project is placed on different solution to avoid problems by simulaneous(multiple) project debugging.
+
+and the solutions can share their dependent library projects.
--- vsproj/vtagSvc/Program.cs
+++ vsproj/vtagSvc/Program.cs
@@ -7,10 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
